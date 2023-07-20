e99d1078ba1c296d88070d059bb089cc5f037a9c
Beka Latsabidze <beka.latsabidze@ens.tsu.edu.ge>
2023-07-20 09:27:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillMember(){
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/skill-member.html'
+        controller: 'SkillMemberController',
+        controllerAs: 'skillMemberCtrl'
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    };
+}
